f7d26adfab9c973ed9739ca073bc5f474dc119fb
Junchao <junchao.zhang@salesforce.com>
2018-02-07 23:32:49 -0800

    about more

--- src/About.js
+++ src/About.js
@@ -5,44 +5,39 @@ import './About.css'
 
 const About = () => {
   return (
-    <article id='about' className='About'>
-      <SectionHeader title='about me' label='01.' />
-      <div className='About-inner'>
-        <section className='About-section parallax-1'>
-          <div className='About-section-parallax' />
+    <article id="about" className="About">
+      <SectionHeader title="about me" label="01." />
+      <div className="About-inner">
+        <section className="About-section parallax-1">
+          <div className="About-section-parallax" />
           <p>
             I bring creative into user experience thoughts and build rich
-            interactive apps that give brands life and add depth to their stories.
+            interactive apps that give brands life and add depth to their
+            stories.
           </p>
         </section>
-        <section className='About-section'>
-          <blockquote className='About-section-quote'>
-            <p>
-              "Imagination is the beginning of creation. You imagine what you
-              desire, you will what you imagine, and at last, you create what you
-              will."
-            </p>
-            <p>-- George Bernard Shaw </p>
-          </blockquote>
-        </section>
-        <section className='About-section parallax-2'>
-          <div className='About-section-parallax' />
+        <blockquote className="About-section About-section__quote">
+          <p>
+            "Imagination is the beginning of creation. You imagine what you
+            desire, you will what you imagine, and at last, you create what you
+            will."
+          </p>
+          <p>-- George Bernard Shaw </p>
+        </blockquote>
+        <section className="About-section parallax-2">
+          <div className="About-section-parallax" />
           <p>
             I'm passionate for all things visual and technical. It is my mission
             to revolutionize the way brands are experienced by making online
             interactions meaningful and memorable.
           </p>
           <p>
-            I use cutting edge web technology combined with pleasing and
-            purposeful visuals to tell stories and invoke a feeling of
-            connectedness.
+            I use cutting edge technology combined with pleasing and purposeful
+            visuals to tell stories and invoke a feeling of connectedness.
           </p>
         </section>
-        <div className='About-more'>
-          <i
-            class='fas fa-chevron-circle-down fa-2x'
-            data-fa-transform='rotate-135'
-          />
+        <div className="About-more">
+          <i class="fas fa-info fa-2x" />
         </div>
       </div>
     </article>
