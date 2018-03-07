a54c40458843f130331911ead8df7b1b44687d4e
Junchao <junchao.zhang@salesforce.com>
2018-03-06 17:53:10 -0800

    portfolio bg

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import './Portfolio.css'
+import project1 from './images/coding.jpg'
 
 import SectionHeader from './SectionHeader'
 
 const Portfolio = () => {
   return (
     <article id="portfolio" className="Portfolio">
-      <SectionHeader title="portfolio" label="02." />
+      <div className="Portfolio-banner">
+        <div className="banner-mask" />
+        <span className="divider" />
+        <div className="banner-mask__white" />
+        <SectionHeader
+          className="Portfolio-header"
+          title="portfolio"
+          label="02."
+        />
+      </div>
       <ul className="Portfolio-list">
         <li className="Portfolio-item">
           <span className="Portfolio-project">
             <figure className="Portfolio-preview">
-              <img />
+              <img className="Portfolio-screenshot" />
             </figure>
             <figcaption className="Portfolio-project-info">
               <h4 className="Portfolio-project-title">project</h4>
--- src/SectionHeader.js
+++ src/SectionHeader.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import cx from 'classnames'
 import './SectionHeader.css'
 
 const SectionHeader = props => {
-  const { label, title } = props
+  const { label, title, className } = props
   return (
-    <h3 className='SectionHeader'>
+    <h3 className={cx('SectionHeader', className)}>
       <label>{label}</label>
       {title}
     </h3>
