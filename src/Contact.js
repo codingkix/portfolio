e25190a08602f4d47ea19fa52150f776772840f6
Junchao <codingkix@gmail.com>
2018-03-20 17:35:08 -0700

    Portfolio (#4)
    * layout
    
    * portfolio bg
    
    * multi project css
    
    * portfolio item

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import SectionHeader from './SectionHeader'
 import './Contact.css'
 
 const Contact = () => {
-  return <article id='contact' className='Contact'>Contact</article>
+  return (
+    <article id="contact" className="Contact">
+      <SectionHeader title="contact" label="03." />
+    </article>
+  )
 }
 
 export default Contact
--- src/SectionHeader.js
+++ src/SectionHeader.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import cx from 'classnames'
 import './SectionHeader.css'
 
 const SectionHeader = props => {
-  const { label, title } = props
+  const { label, title, className, ...rest } = props
   return (
-    <h3 className='SectionHeader'>
+    <h3 className={cx('SectionHeader', className)} {...rest}>
       <label>{label}</label>
       {title}
     </h3>
