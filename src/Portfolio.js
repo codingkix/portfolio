93fe0484ed8c4b9231300f93a34fe225a4f0d1dc
codingkix <codingkix@gmail.com>
2018-03-21 23:49:18 -0700

    project background

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,6 +7,7 @@ import sa from './images/sa.png'
 import redbull from './images/redbull.png'
 import target from './images/target.png'
 import uo from './images/uo.png'
+import dunkin from './images/dunkin.png'
 
 import SectionHeader from './SectionHeader'
 import PortfolioItem from './PortfolioItem'
@@ -14,38 +15,51 @@ import PortfolioItem from './PortfolioItem'
 const projects = [
   {
     screenshot: redbull,
-    title: 'Red Bull Mobile Collect',
-    types: ['web', 'mobile']
+    title: 'RedBull Mobile Collect',
+    types: ['web', 'mobile'],
+    hover: '#1C58A1'
   },
   {
     screenshot: discover,
     title: 'Discover Rewards',
-    types: ['web', 'mobile']
+    types: ['web', 'mobile'],
+    hover: '#DD7030'
   },
   {
     screenshot: kohls,
     title: "Kohl's Yes2You",
-    types: ['web', 'mobile']
+    types: ['web', 'mobile'],
+    hover: '#CC5199'
   },
   {
     screenshot: sa,
     title: 'The League',
-    types: ['web', 'mobile']
+    types: ['web', 'mobile'],
+    hover: '#F66855'
+  },
+  {
+    screenshot: dunkin,
+    title: 'DDPERKS',
+    types: ['web'],
+    hover: '#EFAE36'
   },
   {
     screenshot: target,
     title: 'Target Pharmacy',
-    types: ['web']
+    types: ['web'],
+    hover: '#E83132'
   },
   {
     screenshot: uo,
     title: 'Universal Studio',
-    types: ['web']
+    types: ['web'],
+    hover: '#3E92D3'
   },
   {
     screenshot: grooveshark,
     title: 'GrooveShark Music',
-    types: ['web']
+    types: ['web'],
+    hover: '#4E504E'
   }
 ]
 
--- src/PortfolioItem.js
+++ src/PortfolioItem.js
@@ -1,43 +1,68 @@
 import React from 'react'
 
-const renderTypes = types => {
-  return (
-    <h5 className="Portfolio-project-type">
-      {types.map((type, index) => {
-        if (type === 'mobile') {
+class PortfolioItem extends React.PureComponent {
+  state = {
+    hover: false
+  }
+
+  handleHover = e => {
+    e.preventDefault()
+    this.setState({ hover: true })
+  }
+
+  handleLeave = e => {
+    e.preventDefault()
+    this.setState({ hover: false })
+  }
+
+  renderTypes = types => {
+    return (
+      <h5 className='Portfolio-project-type'>
+        {types.map((type, index) => {
+          if (type === 'mobile') {
+            return (
+              <React.Fragment key={index}>
+                <i className='fas fa-mobile-alt' />
+                <span>Mobile</span>
+              </React.Fragment>
+            )
+          }
           return (
             <React.Fragment key={index}>
-              <i className="fas fa-mobile-alt" />
-              <span>Mobile</span>
+              <i className='fas fa-desktop' />
+              <span>Web</span>
             </React.Fragment>
           )
-        }
-        return (
-          <React.Fragment key={index}>
-            <i className="fas fa-desktop" />
-            <span>Web</span>
-          </React.Fragment>
-        )
-      })}
-    </h5>
-  )
-}
+        })}
+      </h5>
+    )
+  }
 
-const PortfolioItem = ({ item, ...props }) => {
-  const { screenshot, title, types } = item
-  return (
-    <li className="Portfolio-item">
-      <div className="Portfolio-project">
-        <figure className="Portfolio-preview">
-          <img className="Portfolio-screenshot" src={screenshot} alt={title} />
-          <figcaption className="Portfolio-project-info">
-            <h4 className="Portfolio-project-title">{title}</h4>
-            {renderTypes(types)}
-          </figcaption>
-        </figure>
-      </div>
-    </li>
-  )
+  render () {
+    const { screenshot, title, types, hover } = this.props.item
+    const backgroundColor = this.state.hover ? hover : null
+    return (
+      <li className='Portfolio-item'>
+        <div
+          className='Portfolio-project'
+          onMouseEnter={this.handleHover}
+          onMouseLeave={this.handleLeave}
+          style={{ backgroundColor: backgroundColor }}>
+          <figure className='Portfolio-preview'>
+            <img
+              className='Portfolio-screenshot'
+              src={screenshot}
+              alt={title}
+            />
+            <figcaption className='Portfolio-project-info'>
+              <h4 className='Portfolio-project-title'>{title}</h4>
+              {this.renderTypes(types)}
+            </figcaption>
+          </figure>
+        </div>
+      </li>
+    )
+  }
 }
 
 export default PortfolioItem
